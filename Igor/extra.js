6049b12c2c0b42e9ef84bef81882343074d606f7
Igor Y <igoryumashev@gmail.com>
2021-09-29 22:06:32 +0200

    Extra added

--- Igor/extra.js
+++ Igor/extra.js
@@ -1,5 +1,4 @@
-var userNights = 5;
-
+var ID = ['hot1', 'hot2', 'hot3']
 
 class Hotel {
     hotelName;
@@ -14,19 +13,32 @@ class Hotel {
         this.image = image
 
     }
-    showAvailability() {
-
+    showAvailability(uNights, divId) {
+        if (uNights < this.numberOfNights) {
+            document.getElementById(divId).innerHTML = `<em>${this.hotelName}<em>: <button style="font-size: 12pt; color: green;" type="button">I'll reserve</button><br><br>`
+        } else {
+            document.getElementById(divId).innerHTML = `Sorry, no available nights for "<em>${this.hotelName}</em>"<br><br>`
+        }
     }
 }
 
-var random1 = Math.floor(Math.random() * 11);
-var random2 = Math.floor(Math.random() * 11);
-var random3 = Math.floor(Math.random() * 11);
 
-var hotel1 = new Hotel("Gold Crate", random1, "Gold Street 1", 'img');
-var hotel2 = new Hotel("Blue Lagoon", random2, "Blue Street 45", 'img');
-var hotel3 = new Hotel("Red Hell", random3, "Red Street 666", 'img');
+var hotel1 = new Hotel("Golden Dragon", Math.floor(Math.random() * 11), "Gold Street 1", 'img1');
+var hotel2 = new Hotel("Blue Lagoon", Math.floor(Math.random() * 11), "Blue Street 45", 'img2');
+var hotel3 = new Hotel("Red Hell", Math.floor(Math.random() * 11), "Red Street 666", 'img3');
 
-console.log(hotel1)
-console.log(hotel2)
-console.log(hotel3)
+function check() {
+
+    var n = document.getElementById('nights').value;
+
+    hotel1.showAvailability(n, ID[0])
+    hotel2.showAvailability(n, ID[1])
+    hotel3.showAvailability(n, ID[2])
+
+}
+
+document.getElementById('btn').addEventListener('click', check)
+
+console.log(hotel1.numberOfNights)
+console.log(hotel2.numberOfNights)
+console.log(hotel3.numberOfNights)
